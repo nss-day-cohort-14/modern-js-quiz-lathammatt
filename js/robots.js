33e6a8be9f5dc99d42bda63e83a82380fd3e29a4
Matt Latham <latham75@gmail.com>
2016-07-12 11:53:37 -0500

    starting tests and first module

---
+++ js/robots.js
@@ -0,0 +1,13 @@
+"use strict";
+
+
+
+const Robot = function() {
+	this.name=null;
+	this.health=null;
+	this.damage=null;
+	this.weapons=null;
+	this.weaponsNumber = 2;	
+	
+};
+
---
+++ spec/testrunner.js
@@ -0,0 +1,24 @@
+"use strict";
+
+describe ("robot functions", function(){
+
+	it ("should have a base robot function", function(){
+		expect(Robot).toBeDefined();
+	});	
+
+	it ("should have a wedge robot", function(){
+		expect(Wedge).toBeDefined();
+	});	
+
+	it ("should have a crusher robot", function(){
+		expect(Crusher).toBeDefined();
+	});	
+
+	it ("should have a spinner robot", function(){
+		expect(Spinner).toBeDefined();
+	});
+
+
+
+
+})
