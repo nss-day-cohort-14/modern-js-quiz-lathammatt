238f7ec9d3c679c45f01fedc12aeaff9bb86027d
Matt Latham <latham75@gmail.com>
2016-07-21 18:03:27 -0500

    more dom work, player setup

---
+++ js/players.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const weapons = require("./weapons");
+const extras = require("./extras)");
+const warriors = require("./warriors");
+const robots = require("./robots");
+const jquery = require("jquery");
+
+let player1 = null;
+let player2 = null;
+let allbots = $(".model");
+let allweaps = $(".weapons");
+let allext = $(".extra");
+
+
