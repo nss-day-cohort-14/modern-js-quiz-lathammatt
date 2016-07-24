4749daf8e8031a0b50db0ea5c5a675791768144d
Matt Latham <latham75@gmail.com>
2016-07-24 17:06:18 -0500

    weapon selection complete

--- js/players.js
+++ js/players.js
@@ -8,17 +8,18 @@ const jquery = require("jquery");
 
 let player1 = null;
 let player2 = null;
-let toarms = $("#arm");
+let toarms = $("#choose");
+let toexts = $("#armed");
+
 
 toarms.click(userCreate);
+toexts.click(weaponAssign);
 
 
 function userCreate(){
 	let randomwarrior = Math.floor(Math.random()*5+1);
-	console.log("testing");
-	let test = $(".modelbtn:checked").val();
-	console.log("check", test, $("#p1").val());
-	player1 = new warriors.Roster[test]();
+	let selection = $(".modelbtn:checked").val();
+	player1 = new warriors.Roster[selection]();
 	if ($("#p1").val() !== ""){
 	player1.name = $("#p1").val();
 	}
@@ -29,6 +30,16 @@ function userCreate(){
 	console.log("new players", player1,player2);
 	console.log("names",player1.name, player2.name);
 }
+
+function weaponAssign(){
+	let randomweap = Math.floor(Math.random()*7+1);
+	let selection = $(".weapbtn:checked").val();
+	player1.weapon2 = new weapons.Arsenal[selection]();
+	player2.weapon2 = new weapons.Arsenal[Object.keys(weapons.Arsenal)[randomweap]]();
+	console.log("weaps", player1.weapon2, player2.weapon2, player1, player2);
+}
+
+
 
 
 // get selected weapon, add to weapon2
