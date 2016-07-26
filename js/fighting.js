43e203bfd7b6ccff8a9c54ef13b08fc6411c72f7
Matt Latham <latham75@gmail.com>
2016-07-25 22:45:40 -0500

    start fight setup

--- js/fighting.js
+++ js/fighting.js
@@ -3,19 +3,21 @@
 // *****damage vs armor
 // calculate robot damage plus weapon
 // subtract from usage
-// account for accuracy with extras (accuracy, speed) figured in
 // armor of opponent reduces damage
 // 
 //if speed over 25, extra turn
 //if speed under zero, lose every fifth turn
-//weight reduces speed, but increases damage
-//
-//
-//
-//
+
 
 const players = require("./players");
 
-function weaponDmg (attacker, defender){
-	
+
+let wp1 = $("#w1");
+let wp2 = $("#w2");
+
+wp1.click(weaponDmg);
+wp2.click(weapon2dmg);
+
+function weaponDmg(attacker, defender) {
+
 }
